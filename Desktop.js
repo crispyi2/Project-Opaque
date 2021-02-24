4fe3aee0e6e5321b949dde0c3f89ad2f8823e0f8
crispyi2 <abdurtaalib@gmail.com>
2021-02-24 20:46:26 +0000

    mere progress

--- Desktop.js
+++ Desktop.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var serveStatic = require('serve-static')
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
---
+++ Javascript/Index.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("#framewrap")
+        .resizable()
+        .draggable();
+});
