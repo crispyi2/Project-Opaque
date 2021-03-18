d3c7267ceef20abde3c2dfd248837e6e8d6506e4
crispyi2 <abdurtaalib@gmail.com>
2021-03-18 18:20:07 +0000

    looking for a way to get node modules working on the client side

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const Bundler = require('parcel-bundler');
+
+let bundler = new Bundler('input.js')
+bundler.addPackager('foo', require.resolve('./MyPackager'))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
