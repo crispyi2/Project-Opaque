c3972e2296d81e6bdb1784fcf9d4070141044145
Krispiiiii <abdurtaalib@gmail.com>
2021-03-23 20:39:22 +0000

    starting over project was messy and wasnt going anywhere

--- public/javascripts/System-Checker.js
+++
@@ -1,5 +0,0 @@
-var fs = require(["fs"]);
-const dirTree = require(["directory-tree"]);
-
-const tree = dirTree('./scripts/');
-console.log(tree);
--- scripts/System-Checker.js
+++
@@ -1,7 +0,0 @@
-import '/bundle.js'
-var fs = require('fs');
-const testFolder = '/';
-
-fs.readdirSync(testFolder).forEach(file => {
-  console.log(file);
-});
