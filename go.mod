58fa8ff30332c937a80561538499fb4a53843feb
crispyi2 <abdurtaalib@gmail.com>
2021-03-25 19:05:03 +0000

    go

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
-module "github.com/crispyi2/Project-Opaque"
+module github.com/crispyi2/Project-Opaque
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,32 +1,26 @@
-// This is the name of our package
-// Everything with this package name can see everything
-// else inside the same package, regardless of the file they are in
 package main
 
-// These are the libraries we are going to use
-// Both "fmt" and "net" are part of the Go standard library
 import (
-	// "fmt" has methods for formatted I/O operations (like printing to the console)
+	// Import the gorilla/mux library we just installed
 	"fmt"
-	// The "net/http" library has methods to implement HTTP clients and servers
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	// The "HandleFunc" method accepts a path and a function as arguments
-	// (Yes, we can pass functions as arguments, and even treat them like variables in Go)
-	// However, the handler function has to have the appropriate signature (as described by the "handler" function below)
-	http.HandleFunc("/", handler)
+	// Declare a new router
+	r := mux.NewRouter()
 
-	// After defining our server, we finally "listen and serve" on port 8080
-	// The second argument is the handler, which we will come to later on, but for now it is left as nil,
-	// and the handler defined above (in "HandleFunc") is used
-	http.ListenAndServe(":8080", nil)
+	// This is where the router is useful, it allows us to declare methods that
+	// this path will be valid for
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// We can then pass our router (after declaring all our routes) to this method
+	// (where previously, we were leaving the second argument as nil)
+	http.ListenAndServe(":8080", r)
 }
 
-// "handler" is our handler function. It has to follow the function signature of a ResponseWriter and Request type
-// as the arguments.
 func handler(w http.ResponseWriter, r *http.Request) {
-	// For this case, we will always pipe "Hello World" into the response writer
 	fmt.Fprintf(w, "Hello World!")
 }
