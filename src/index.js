87799a46f85ae3d32f3ae0bfe8e34af14e66cc53
crispyi2 <abdurtaalib@gmail.com>
2021-03-25 17:01:37 +0000

    e

--- src/index.js
+++ src/index.js
@@ -53,26 +53,5 @@ require.context(
     /.*/ // RegExp
 )("./" + expr + "")
 
-var _getAllFilesFromFolder = function(dir) {
-
-    var filesystem = require("fs");
-    var results = [];
-
-    filesystem.readdirSync(dir).forEach(function(file) {
-
-        file = dir+'/'+file;
-        var stat = filesystem.statSync(file);
-
-        if (stat && stat.isDirectory()) {
-            results = results.concat(_getAllFilesFromFolder(file))
-        } else results.push(file);
-
-    });
-
-    return results;
-
-};
-
-
 
 document.body.appendChild(component());
