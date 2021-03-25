e849d4bd456f6f87889707dddd0581cee55cd27a
crispyi2 <abdurtaalib@gmail.com>
2021-03-25 19:28:36 +0000

    e

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/crispyi2/Project-Opaque
-
-require github.com/gorilla/mux v1.8.0 // indirect
---
+++ index.js
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+
+const fs = require("fs")
+
+fs.mkdir("./src", function(err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log("New directory successfully created.")
+  }
+})
