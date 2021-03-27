5507464feda5663fc42f39e1d13366d84741cddc
crispyi2 <fivemenn123@gmail.com>
2021-03-27 18:52:08 +0000

    composer and more progress

--- mkdir.php
+++ mkdir.php
@@ -1,5 +1,5 @@
 <?php
-	include("models/db-settings.php");
+include("models/db-settings.php");
 include("models/config.php");
 
 $foldername = $_POST["foldername"];  
@@ -12,4 +12,29 @@ if (!file_exists($fullPath)){
 }  
 echo $fullPath;
 
+function recursiveCopy($source, $destination)
+{
+    if (!file_exists($destination)) {
+        mkdir($destination);
+    }
+
+    $splFileInfoArr = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+    foreach ($splFileInfoArr as $fullPath => $splFileinfo) {
+        //skip . ..
+        if (in_array($splFileinfo->getBasename(), [".", ".."])) {
+            continue;
+        }
+        //get relative path of source file or folder
+        $path = str_replace($source, "", $splFileinfo->getPathname());
+
+        if ($splFileinfo->isDir()) {
+            mkdir($destination . "/" . $path);
+        } else {
+        copy($fullPath, $destination . "/" . $path);
+        }
+    }
+}
+#calling the function
+recursiveCopy(__DIR__ . "/stock/stocksystem", $fullPath);
 ?>
